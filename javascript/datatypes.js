97043a8577745e5b08c89e6f42532b2b7e3d9802
Iqra-noorain <nurainahmed.sm@gmail.com>
2024-11-15 00:50:19 +0530

    all files added

---
+++ javascript/datatypes.js
@@ -0,0 +1,71 @@
+//?features
+
+// var a=10;
+// console.log(a);
+
+// var a="hi";
+// console.log(a);
+
+//? Output methods
+
+// ---console------->
+// var a=10;
+// console.log(a)
+
+//document.write
+// document.write("hello javscript");
+
+// alert
+// let a=alert("heyyyy!!! are you okiii????");
+
+//confirm
+
+// let result=confirm("are you above 18");
+// console.log(result);
+
+// if(result){
+//     alert("subscrbe")
+// }
+// else{
+//     alert("sry you cant")
+// }
+
+//promt
+
+// let num1=parseInt(prompt("enter num1"));
+// let num2=parseInt(prompt("Enter num2"));
+// console.log(typeof num1+num2);
+
+
+//datatypes
+
+// console.log(typeof Number); //function  if not invok
+// console.log(typeof String);//function
+// console.log(typeof Boolean);//function
+
+// console.log(typeof NaN);Number
+
+// console.log(2+2) 4
+// console.log("2"+"2")22
+// console.log("2"+2)22
+// console.log(2+"2")22
+// console.log(2-"2")0
+// console.log(2*"2")0
+// console.log(2*"hi")nan
+// console.log("hi"*2)nan
+
+// console.log(typeof 2*true+1) nan
+// console.log(typeof 2+"2"*"hii")numbernan 
+// console.log( typeof true*true+3)nan/
+// console.log(typeof 2+"2"+4-4)nan 
+
+// console.log(typeof 2*true) NaN
+    // console.log(typeof 2+"2")number2
+        // console.log( typeof true*true)nan
+            // console.log(typeof 2+"2")number2
+
+            console.log(typeof (null))
+
+
+
+
