f1b2f6c57856f9d312840b06d6fb765f8b727183
Iqra-noorain <nurainahmed.sm@gmail.com>
2024-11-20 23:14:45 +0530

    practiced dom methods

---
+++ javascript/day1.js
@@ -0,0 +1,21 @@
+let section=document.getElementById("section")
+// console.log(section)
+
+// let h1=document.getElementsByClassName("h1")
+// console.log(h1)  //[h1.h1, h1.h1]/
+// console.log(h1[1]) //am the first 
+
+// let tagname= document.getElementsByTagName("div")
+// console.log(tagname)[div#div, div# div, div#demo, div: div#div, " div": div# div, demo: div#demo]
+// console.log(tagname[5])  //^ undefnd
+
+// let selector=document.querySelector("#div")
+// console.log(selector)  // first occurnce
+
+// let selectorall=document.querySelectorAll("div")
+// console.log(selectorall[3])  //^if the index is not avaialble then undeined
+console.log(section.innerHTML)
+
+console.log(section.innerText)
+
+console.log(section.textContent)
